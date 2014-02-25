533e43d183c19eea46f13ed3b5bf6d34da6cf86e
lancelot <cja.china@gmail.com>
2014-02-25 11:25:59 +0800

    js

---
+++ js/adjustment/js/mv-adjustment.js
@@ -0,0 +1,91 @@
+﻿/**
+ * Created by Administrator on 14-1-27.
+ */
+(function (jQuery,exports) {
+    jQuery.fn.mvAdjustment = function (origin,adjustNumber,option) {
+        origin = origin||[];
+        var self = $(this),
+            adjustInput = $("<input />").addClass("adjust-value"),
+            adjustDiv = $("<div></div>").addClass("adjustment-button"),
+            adjustButtonUp = $("<span></span>").addClass("button-up"),
+            adjustButtonDown = $("<span></span>").addClass("button-down"),
+            figures,
+            valueRex,
+            adjustLength,
+            beforePlaceholder,
+            afterPlaceholder;
+
+        if(origin === "getData"){
+            return checkValue(self.find(".adjust-value")[0].value);
+        }
+        valueRex = self[0].valueRex = "(\\" + (option[1]?option[1]:"s*") + ")" + "(\\-*\\d*\\.{0,1}\\d*)"
+            + "(\\" + (option[2]?option[2]:"s*") + ")";
+        adjustLength = (option[0]?option[0]:option).toString().length;
+        beforePlaceholder = option[1]?option[1]:"";
+        afterPlaceholder= option[2]?option[2]:"";
+        figures = (function (option){
+            var  figuresNumber;
+            if (typeof option !== "number"){
+                figuresNumber = option[0].toString();
+            }else{
+                figuresNumber = option.toString();
+            }
+            figuresNumber = figuresNumber.length - figuresNumber.indexOf(".") - 2;
+            return figuresNumber;
+        })(option) + 1;
+            adjustInput.attr("realValue",origin);
+            adjustDiv.append(adjustButtonUp,adjustButtonDown);
+            self.append(adjustInput,adjustDiv);
+
+        adjustInput[0].value = beforePlaceholder + placeholderZero(origin) + afterPlaceholder;
+
+
+        function checkValue (Value) {
+
+            var valueRule = new RegExp(self[0].valueRex),
+                valueArr;
+
+            valueArr = Value.match(valueRule);
+
+            return valueArr?Number(valueArr[2]):""
+        }
+
+        function createZero(number){
+            var zero = "";
+            for (var i = 0;i < number;i++){
+                zero = zero + "0"
+            }
+            return zero;
+        }
+
+        function placeholderZero(Value){
+            Value = Number(Value).toFixed(figures);
+
+            var numberString = Value.toString(),
+                isNeed = numberString.indexOf(".");
+
+            if (isNeed !== -1){
+
+                if (adjustLength === numberString.length || numberString.indexOf("-") === 0) {
+                    return Value;
+                }
+                Value = Value + createZero(adjustLength - numberString);
+                return Value;
+            }
+            Value = Value + "." +createZero(figures);
+
+            return Value;
+        }
+
+        adjustButtonUp.on("click",function () {
+            var inputValue = checkValue(adjustInput[0].value);
+            adjustInput[0].value =  beforePlaceholder + placeholderZero(inputValue + adjustNumber) + afterPlaceholder;
+        });
+        adjustButtonDown.on("click",function () {
+            var inputValue = checkValue(adjustInput[0].value);
+            adjustInput[0].value =  beforePlaceholder + placeholderZero(inputValue - adjustNumber) + afterPlaceholder;
+        });
+
+
+    } 
+})($,window);
---
+++ js/progress-bar/js/progress-bar.js
@@ -0,0 +1,64 @@
+﻿/**
+ * Created by Administrator on 14-1-27.
+ */
+(function (jQuery,exports) {
+
+    jQuery.fn.mvLoadIng = function (start,callback) {
+        var loadState = 0 + start,
+            stateSave,
+            self = this;
+        (function loading (){
+            stateSave = (100 - loadState)/2
+            loadState = loadState + stateSave;
+            callback (loadState);
+            self[0].loadIng = setTimeout(function () {
+                loading();
+            },500)
+        })();
+    }
+
+    jQuery.fn.mvLoadStart = function mvLoadStart (start) {
+        var self = $(this),
+            loadStateBox = $("<div></div>").addClass("mv-load-state"),
+            loadIng = $("<div></div>").addClass("mv-load-ing"),
+            loadP = $("<p></p>").html("数据加载中"),
+            load = $("<div></div>").addClass("mv-load"),
+            loadStyle = $("<div></div>").addClass("mv-load-style"),
+            loadCenter = $("<div></div>").addClass("mv-progress-bar-center"),
+            state = start || 20;
+
+        self.addClass("mv-center-progress");
+        load.append(loadStyle);
+        loadIng.append(loadP,load);
+        loadStateBox.append(loadIng);
+        loadCenter.append(loadStateBox);
+
+        self.append(loadCenter);
+
+
+        loadStateBox.mvLoadIng(state,function (loadState) {
+            var self = jQuery(loadStateBox),
+                selfLoad = self.find(".mv-load-style");
+            self.fadeIn();
+            selfLoad.animate({
+                "width": (loadState * 2.1) + "px" 
+            });
+        });
+    };
+    jQuery.fn.mvLoadEnd = function mvLoadEnd (speed) {
+        var self = $(this),
+            loadStateBox = self.find(".mv-progress-bar-center"),
+            selfLoad = self.find(".mv-load-style"),
+            speed = speed || 1000;
+
+        selfLoad.stop();    
+       
+        clearTimeout(loadStateBox[0].loadIng);
+
+        loadStateBox.fadeOut(speed,function () {
+            loadStateBox.remove();
+        });
+
+    };
+
+}) ($,window);
